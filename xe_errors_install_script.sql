18b2c63c697ffa46776e465de699b44761c755d4
Bruce <39556305+BruceSpruce@users.noreply.github.com>
2023-08-26 20:52:26 +0200

    Add 9104

--- xe_errors_install_script.sql
+++ xe_errors_install_script.sql
@@ -10,7 +10,7 @@ GO
 CREATE EVENT SESSION [Errors] ON SERVER 
 ADD EVENT sqlserver.error_reported(
     ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.query_hash,sqlserver.session_id,sqlserver.sql_text,sqlserver.transaction_id,sqlserver.username)
-    WHERE ([severity]>(10))) 
+    WHERE (([package0].[greater_than_int64]([severity],(10))) AND ([package0].[not_equal_int64]([error_number],(9104)))))
 ADD TARGET package0.event_file(SET filename=N'C:\XE\errors_queries.xel',metadatafile = N'C:\XE\errors_queries.xem',max_file_size=(20),max_rollover_files=(10))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=5 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=ON)
 GO
